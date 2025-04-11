96b5b3e57bdadd2cec0225ba8e120fc2260a1bea
Tecknicalle <joao.p.garcia.melo@gmail.com>
2025-04-10 21:37:53 -0300

    inicia o repositório

---
+++ atividade.py
@@ -0,0 +1,22 @@
+# atividade 01 
+# data 10-04-2025
+
+
+
+
+
+
+
+
+
+
+
+
+
+valor_camisa = 83.0
+valor_do_desconto = preço * (13/100)
+vl_final = preço - valor_do_desconto
+valor = 72.21
+desconto_final = 10.79
+
+print(valor_camisa - valor_do_desconto)
